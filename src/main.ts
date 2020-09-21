faaef464d763c8cb45976eed521e8fccf1151333
akanass <akanass@icloud.com>
2020-09-21 17:02:09 +0200

    step-02-solution

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({ logger: true }),
   );
   await app.listen(3000);
+  Logger.log(`Application served at http://localhost:3000`, 'bootstrap');
 }
 
 bootstrap();
