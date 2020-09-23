f5ee824974589bdb22fe548f928c522a899b0454
akanass <akanass@icloud.com>
2020-09-23 19:37:39 +0200

    step-15-solution

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HelloModule } from './hello/hello.module';
 import { PeopleModule } from './people/people.module';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import * as Config from 'config';
 
 @Module({
-  imports: [ HelloModule, PeopleModule ],
+  imports: [
+    HelloModule,
+    PeopleModule,
+    MongooseModule.forRoot(Config.get<string>('mongodb.uri'), Config.get<MongooseModuleOptions>('mongodb.options')),
+  ],
 })
 export class AppModule {
 }
